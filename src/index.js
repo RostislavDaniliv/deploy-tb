2e13dde786f283c80918ad23c30b08105e5c8649
rostik <daniliv.rostik624@gmail.com>
2021-05-19 10:13:23 +0300

    sec

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
 import App from './App/App'
-import { history } from './helpers/history'
 import './api/axiosInterceptors'
 
 
 ReactDOM.render(
-  <Router history={history}>
+  <Router>
       <Provider store={store}>
           <App />
       </Provider>
